588814243156249a39c6d18e0ceefcff48d4a582
surendarg97 <surendarg97@gmail.com>
2021-05-30 22:02:57 +0530

    WGSCopy commit

--- WGSCOPY/WGSCOPY/src/test/java/com/qa/sample/AppTest.java
+++ WGSCOPY/WGSCOPY/src/test/java/com/qa/sample/AppTest.java
@@ -1,8 +1,8 @@
 package com.qa.sample;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -16,5 +16,6 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+        System.out.println("Hello world");
     }
 }
